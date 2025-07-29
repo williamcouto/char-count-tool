11b09b90ec34121100b98ca47acb0c7689d1eb3f
williamcouto <willplacido40@gmail.com>
2025-07-29 19:53:28 -0300

    Feat: Inserindo a lógica da contagem de caracteres; acessando o tamanho dos valores inseridos

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 let textarea = document.getElementById('user-textarea')
+let char = document.getElementById('stat-char')
+
+// Contagem de caracteres
+function countCharacter(){
+     let textareaUser = textarea.value.length
+     char.textContent = textareaUser
+}
+textarea.addEventListener('input', () => {
+    countCharacter()
+})
