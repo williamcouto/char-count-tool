2f8dc96bebae0fcf2f2c1ce55cb90631fee4680e
williamcouto <willplacido40@gmail.com>
2025-07-30 20:33:13 -0300

    Feat: Inserindo a funcionalidade de contagem de palavras - Utilizando metodos de arrays para manipular o campo de texto.

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 let textarea = document.getElementById('user-textarea')
+let words = document.getElementById('stat-words')
 let char = document.getElementById('stat-char')
 
 // Contagem de caracteres
-function countCharacter(){
-     let textareaUser = textarea.value.length
-     char.textContent = textareaUser
+function countCharacter() {
+    let textareaUser = textarea.value.length
+    char.textContent = textareaUser
 }
+
+// Contagem de Palavras
+function countWords() {
+    let wordUser = textarea.value
+    // Verifica se o campo de texto está vazio
+    if(wordUser === ""){
+        words.textContent = 0
+        return
+    }
+
+    let wordsArray = wordUser.split(/\s+/)
+    let filteredWords = wordsArray.filter(word => word.length > 0)
+    words.textContent = filteredWords.length
+}
+
 textarea.addEventListener('input', () => {
     countCharacter()
+    countWords()
 })
