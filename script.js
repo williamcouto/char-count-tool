6fd71b3b24cd4e01e40591355d6068df835eb3cc
williamcouto <willplacido40@gmail.com>
2025-07-31 21:12:17 -0300

    Feat: Criando a função para realizar a limpeza do campo de texto; apagando e atualizando os valores estatisticos

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let textarea = document.getElementById('user-textarea')
 let words = document.getElementById('stat-words')
 let char = document.getElementById('stat-char')
+let btnClear = document.getElementById('btn-clear')
+
+// Limpar a area de texto
+function clearTextarea(){
+    textarea.innerHTML = ''
+    words.innerHTML = '0'
+    char.innerHTML = '0'
+}
 
 // Contagem de caracteres
 function countCharacter() {
@@ -26,3 +34,7 @@ textarea.addEventListener('input', () => {
     countCharacter()
     countWords()
 })
+
+btnClear.onclick = () => {
+    clearTextarea()
+}
