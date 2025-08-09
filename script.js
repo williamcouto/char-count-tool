4a07ea6bd61b2beecd32de1f8fddf4d5e98e3a9a
williamcouto <willplacido40@gmail.com>
2025-08-09 17:34:04 -0300

    Feat: Removendo funções de notificações e estruturando uma instância para unificar todas as notificações futuras

--- script.js
+++ script.js
@@ -58,8 +58,7 @@ function countLines() {
 //Contagem de Números
 function countNumberStat() {
     let numsTextarea = textarea.value
-    let regexNum = new RegExp(/\d/g)
-    //expressão para aceitar cada digito individualmente
+    let regexNum = new RegExp(/\d/g) //expressão para aceitar cada digito individualmente
     let foundNums = numsTextarea.match(regexNum)
 
     //Para evitar erro causado por acessar o tamanho de um elemento null
@@ -79,7 +78,7 @@ function updateProgressBar(textareaUser) {
 
     if (percent > 100) {
         currentNum.classList.add('warning-limit')
-        getLimitNotification()
+        
     }
     else {
         currentNum.classList.remove('warning-limit')
@@ -87,39 +86,27 @@ function updateProgressBar(textareaUser) {
 
     // Calculo de caracteres restantes
     let remainChar = limitNum - textareaUser
-    if(remainChar == limitNum / 2){
-        getWarning(remainChar)
-    }
+    
 }
 
-// Notificação para caracteres restantes
-function getWarning(remainChar){
-    let notificationChar = new Notyf()
-    notificationChar.error({
-        message: `Atenção: Resta ${remainChar} caracteres` ,
-        duration: 2100,
-        position: {
-            x : "center",
-            y: "top"
+let textNotification = new Notyf({
+    duration: 2100,
+    position: {
+        x: "center",
+        y: "top"
+    },
+    types: [
+        {
+            type: "error",
+            background: "#DC2626"
         },
-        background: "#F59E0B"
-    }) 
-}
+        {
+            type: "warning",
+            background: "#F59E0B"
+        }
+    ]
+})
 
-
-// Notificação para limite excedido
-function getLimitNotification(){
-    let notificationAlert = new Notyf()
-    notificationAlert.error({
-        message: "O limite de caracteres foi atingido!",
-        duration: 2000,
-        position: {
-            x: "center",
-            y: "top"
-        },
-        background: "#DC2626"
-    })
-}
 
 select.addEventListener('change', (Event) => {
     const newLimit = Event.target.value
